ca1bd4f16ba6b86e808651a1a02a863c2a5a93ef
luquet <luccasaguilherme@gmail.com>
2025-08-11 01:08:25 -0300

    mais exercicio

---
+++ Estoque/src/application/Main.java
@@ -0,0 +1,62 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+
+		Product product = new Product();
+		System.out.println("Enter Product data :  ");
+		System.out.print("Name: ");
+		product.name = sc.nextLine();
+		System.out.println("Price: ");
+		product.price = sc.nextDouble();
+		System.out.print("Quantity in Stock:");
+		product.quantity = sc.nextInt();
+		
+		System.out.println();
+		System.out.println("Product data: " + product);
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be added in stock:" );
+		int quantity = sc.nextInt();
+		product.addProducts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		
+		System.out.println();
+		System.out.println("Enter the number of products to be removed from stock:" );
+		quantity = sc.nextInt();
+		product.removeProcuts(quantity);
+		
+		System.out.println();
+		System.out.println("Update data: " + product);
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		sc.close();
+	}
+
+}
---
+++ Triangulo/src/application/Main.java
@@ -0,0 +1,47 @@
+package application;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Triangulo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		Triangulo x,y;
+		x = new Triangulo ();
+		y = new Triangulo ();
+		
+		
+		System.out.println("Entre com as medidas do triangulo X: ");
+		x.a = sc.nextDouble();
+		x.b = sc.nextDouble();
+		x.c = sc.nextDouble();
+		System.out.println("Entre com as medidas do triangulo Y: ");
+		y.a = sc.nextDouble();
+		y.b = sc.nextDouble();
+		y.c = sc.nextDouble();
+		
+		
+		double areaX = x.area();
+		double areaY = y.area();
+		
+		
+		System.out.printf("Area do Triangulo X : %.4f%n " , areaX);
+		System.out.printf("Area do Triangulo Y : %.4f%n " , areaY);
+		
+		
+		if ( areaX > areaY) {
+			System.out.println ("A largura da area : X" );
+		}
+		else {
+			System.out.println ("A largura da area : Y" );
+		}
+		sc.close();
+
+	}
+
+}
